7252cfe5702c6f5b8d8696b45134245b9ada3141
Brandon Mendoza Niño <118572820+BrandonM930@users.noreply.github.com>
2023-11-01 13:10:48 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,78 +1,105 @@
-import tkinter as tk
-from tkinter import ttk
-import time
-from tkinter import Canvas, Scale
-from googletrans import Translator
-from automaton_logic import PDA
-import pyttsx3
+import tkinter as tk  # Importa la librería tkinter para construir la interfaz gráfica
+from tkinter import ttk  # Importa ttk, que proporciona widgets con un estilo mejorado
+import time  # Importa la librería time para manejar pausas en el programa
+from tkinter import Canvas, Scale  # Importa elementos gráficos de tkinter
+from googletrans import Translator  # Importa el traductor de texto de googletrans
+from automaton_logic import PDA  # Importa la lógica del autómata de pila
+import pyttsx3  # Importa pyttsx3 para la funcionalidad de texto a voz
 
+# Define una clase llamada PDAGraphicalInterface que manejará la interfaz gráfica
 class PDAGraphicalInterface:
     def __init__(self, master):
+        # Inicializa la ventana principal de la aplicación
         self.master = master
         self.master.title("PDA Visualization")
         
-        # Create a PDA instance
+        # Crea una instancia del autómata de pila
         self.pda = PDA()
         
+        # Inicializa el traductor de idiomas
         self.translator = Translator()
         self.current_language = "en"
         
-        self.languages = {"es": "Spanish", "en": "English", "fr": "French"}
+        # Define los idiomas disponibles
+        self.languages = { "en": "English", "es": "Spanish", "fr": "French"}
         
+        # Inicializa la funcionalidad de texto a voz
         self.init_text_to_speech()
         
-        # Create a dropdown menu to select the language
+        # Crea una etiqueta para seleccionar el idioma
         self.language_label = ttk.Label(master, text=self.translate("Select a language:"))
         self.language_label.pack()
         
+        # Crea un cuadro combinado para seleccionar el idioma
         self.language_selector = ttk.Combobox(master, values=list(self.languages.values()), state="readonly")
         self.language_selector.current(0)
         self.language_selector.pack()
         self.language_selector.bind("<<ComboboxSelected>>", self.change_language)
         
+        # Crea una etiqueta para la entrada de texto
         self.input_label = tk.Label(master, text=self.translate("Enter an expression to validate if it's an even-length palindrome"))
         self.input_label.pack()
 
+        # Crea un campo de entrada de texto
         self.input_entry = tk.Entry(self.master)
         self.input_entry.pack()
 
+        # Crea una etiqueta para mostrar el resultado
         self.result_label = tk.Label(master, text="")
         self.result_label.pack()
 
+        # Crea un botón para validar la entrada
         self.evaluate_button = tk.Button(self.master, text=self.translate("VALIDATE"), command=self.validate_input)
         self.evaluate_button.pack()
         
-        self.speed_scale = Scale(self.master, label="", from_=1, to=10, orient=tk.HORIZONTAL)
+        # Crea una escala para ajustar la velocidad de visualización
+        self.speed_scale = Scale(self.master, label="Speed", from_=1, to=10, orient=tk.HORIZONTAL)
         self.speed_scale.set(5)
         self.speed_scale.pack()
 
+        # Crea una etiqueta para mostrar los símbolos evaluados
         self.evaluated_symbol = tk.Label(master, text="")
         self.evaluated_symbol.pack()
 
+        # Crea un lienzo para visualizar el autómata de pila
         self.canvas = Canvas(self.master, width=800, height=600)
         self.canvas.pack()
        
+        # Visualización inicial del autómata de pila
         self.visualize_pda()
 
+        # Inicialización de la variable de control para el bucle principal
         self.running = False
-        
+
     def init_text_to_speech(self):
+        # Inicializa el motor de texto a voz
         self.engine = pyttsx3.init()
 
     def change_language(self, event):
+        # Cambia el idioma actual en respuesta a la selección del usuario
         selected_language = list(self.languages.keys())[list(self.languages.values()).index(self.language_selector.get())]
         self.current_language = selected_language
         self.update_ui_language()
 
-        # Solicitar entrada en el idioma seleccionado
-        self.speak_text(self.translate("Please enter an expression to validate if it's an even-length palindrome"))
+        # Solicita entrada en el idioma seleccionadon to v
+        self.speak_text(self.translate("Please enter an expressioalidate if it's an even-length palindrome"))
 
     def update_ui_language(self):
+        # Actualiza la interfaz de usuario con el idioma seleccionado
         self.language_label.config(text=self.translate("Select a language:"))
         self.input_label.config(text=self.translate("Enter an expression to validate if it's an even-length palindrome"))
         self.evaluate_button.config(text=self.translate("VALIDATE"))
 
+        # Traduce la etiqueta de la escala de velocidad
+        speed_label_text = self.translate("Speed")
+        self.speed_scale.config(label=speed_label_text)
+        
+        # Traduce la variable stack_text
+        stack_text = self.translate("Stack:")
+        self.draw_stack_label.config(text=stack_text)
+
     def validate_input(self):
+        # Valida la entrada de texto
         self.result_label.config(text="-")
         input_string = self.input_entry.get()
         self.pda.reset()
@@ -116,12 +143,17 @@ class PDAGraphicalInterface:
         time.sleep(5 / speed) 
 
     def visualize_pda(self, input_symbol=None):
+        # Limpia el lienzo
         self.clear_canvas()
+        # Dibuja los nodos (estados)
         self.draw_nodes()
+        # Dibuja las transiciones
         self.draw_transitions(input_symbol)
+        # Dibuja la pila
         self.draw_stack()
 
     def clear_canvas(self):
+        # Borra todos los elementos en el lienzo
         self.canvas.delete("all")
 
     def draw_nodes(self):
@@ -132,6 +164,7 @@ class PDAGraphicalInterface:
             self.draw_node(x, y, fill_color, state, border_width)
 
     def draw_node(self, x, y, fill_color, state, border_width):
+        # Dibuja un nodo (estado)
         self.canvas.create_oval(x - 20, y - 20, x + 20, y + 20, fill=fill_color, width=border_width)
         self.canvas.create_text(x, y, text=state)
 
@@ -143,6 +176,7 @@ class PDAGraphicalInterface:
             self.draw_transition(current_state, new_state, f"{symbol if symbol != '' else 'λ'}, {stack_symbol} -> {''.join(push_symbols if push_symbols != '' else 'λ')}", color)
 
     def draw_stack(self):
+        # Dibuja la representación gráfica de la pila en el lienzo
         stack_text = self.translate("Stack:")
         for i, symbol in enumerate(self.pda.stack[::-1]):
             x, y = 600, 100 + i * 30
@@ -152,6 +186,7 @@ class PDAGraphicalInterface:
         self.canvas.create_text(600, 100 + len(self.pda.stack) * 30 + 20, text=stack_text, font=('Helvetica', 12), anchor=tk.W)
 
     def draw_transition(self, current_state, new_state, label, color):
+        # Dibuja una transición en el lienzo
         x1, y1 = self.get_node_coordinates(current_state)
         x2, y2 = self.get_node_coordinates(new_state)
 
@@ -165,6 +200,7 @@ class PDAGraphicalInterface:
         self.canvas.create_text(label_x, label_y, text=label, fill=color)
 
     def calculate_label_position(self, x1, y1, x2, y2):
+        # Calcula la posición de la etiqueta de la transición
         label_x = (x1 + x2) / 2
         label_y = y1 - 30
 
@@ -177,13 +213,14 @@ class PDAGraphicalInterface:
         return label_x, label_y
 
     def get_node_coordinates(self, state):
+        # Obtiene las coordenadas de un nodo (estado) en el lienzo
         state_number = int(state[1:])
         x = 100 + state_number * 150
         y = 300
         return x, y
 
     def run(self):
-        # Run the main loop
+        # Ejecuta el bucle principal
         self.running = True
         while self.running:
             try:
@@ -194,13 +231,16 @@ class PDAGraphicalInterface:
                 break
 
     def translate(self, text):
+        # Traduce el texto a otro idioma
         return self.translator.translate(text, src="en", dest=self.current_language).text
 
     def speak_text(self, text):
+        # Convierte texto en voz y lo reproduce
         self.engine.say(text)
         self.engine.runAndWait()
 
 if __name__ == "__main__":
+    # Inicia la aplicación creando una instancia de la clase PDAGraphicalInterface y ejecutando su bucle principal
     root = tk.Tk()
     app = PDAGraphicalInterface(root)
     app.run()
